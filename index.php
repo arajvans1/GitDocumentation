acc01c3bb25b46c697d4ad57923134831b3266c0
Amit Rajvanshi <arajvans@Amits-MacBook-Pro.local>
2022-05-19 00:54:29 +0530

    adding new repo
    adding git tutorial free freecodecamp.org

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <title> Git Documentation!! </title> 
+        <link type="text/css" rel="stylesheet" href="base.css">
+    </head>
+    <body>
+        <nav>
+            <ul>
+                <li><a href="index.php" class="back">&lArr; Home</a></li>
+                <li><a href="tbd.php" class="forward"> TBD &rArr;</a></li>
+            </ul>
+        </nav>
+        <h1>Git Commands</h1>
+        <pre>
+        repo -> repository
+        clone -> bring a repo down from the internet (remote repository like Github) to your local machine
+        add -> track your files and changes with Git
+        commit -> save your changes into Git
+        push -> push your changes to your remote repo on Github (or another website)
+        pull -> pull changes down from the remote repo to your local machine
+        status -> check to see which files are being tracked or need to be commited
+        init -> use this command inside of your project to turn it into a Git repository and start using Git with that codebase 
+
+        git push origin master -> to push changes to Github
+        git push -u origin master -> in future we will be able to just say git push to push the 
+                  changes to remote repository. -u is for upstream.
+        </pre>
+    
+    </body>
+</html>
